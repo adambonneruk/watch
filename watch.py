312be854622ee9e5e4cfbadbb36f1f5c986c50f9
adambonneruk <adam@bonner.uk>
2021-09-24 19:37:55 +0100

    used dictionary to leverage colourise

--- watch.py
+++ watch.py
@@ -1,5 +1,8 @@
 """watch a given directory for file changes using the command line, useful for demos"""
 import os, sys, re, time
+from colour import colourise
+
+tick = 0.25
 
 def full_path(path):
     """fixes /'s and used to expand an initial path component ~ to user’s home directory"""
@@ -7,47 +10,83 @@ def full_path(path):
 
     return full_path
 
-def get_filepaths(path):
+def scan_filepaths(path):
     """navigate the supplied path and return all files (including hidden) as list"""
     filepaths = []
 
     for root, dirs, files in os.walk(path):
         for file in files:
-            filepaths.append(os.path.join(root,file))
+            filepath = os.path.join(root,file)
+            filepaths.append(filepath[len(path)+1:])
 
     return filepaths
 
-def print_filepaths(req_path, filepaths):
-    """pretty print a list of filepaths removing the "root" req_path filepath"""
-    for filepath in filepaths:
-        pretty_path = filepath[len(req_path):]
-        print(pretty_path)
+def pretty_print(pas):
+    os.system("cls")
+    for path, status in pas.items():
+        if status == "add":
+            print(colourise(path,"green"))
+        elif status == "del":
+            print(colourise(path,"red"))
+        elif status == "gon":
+            print(colourise(path,"grey"))
+        else:
+            print(colourise(path,"white"))
 
-    return None
+def update_path_and_statuses(path_and_status, current_paths, new_paths):
+    # look into the dictionary...
+    for path, status in path_and_status.items():
+        # ...remove really old items (anything that was removed last time)
+        if status == "del":
+            path_and_status.update({path:"gon"})
+        # ...set everything else now to removed
+        if status == "nod":
+            path_and_status.update({path:"del"})
+
+    # look at the new filepath array (path_delta), what is nod and add?
+    for new_path in new_paths:
+        if new_path in current_paths:
+            path_and_status.update({new_path:"nod"}) # both lists, so no diference
+        else:
+            path_and_status.update({new_path:"add"}) # only new, so add
+
+    return path_and_status
 
 def main():
-    """main function, wrapped in a try function to control ctrl+c keyboard excpetion while sleeping"""
-    try:
-        while True:
-            os.system("cls")
+    """main function, wrapped in a try function to control ctrl+c keyboard exception while sleeping"""
 
-            if (len(sys.argv) > 1):
-                req_path = full_path(sys.argv[1])
-            else:
-                req_path = full_path(os.getcwd())
+    # if theres a path passed to the function, use it, otherwise use the current working directory
+    if (len(sys.argv) > 1):
+        req_path = full_path(sys.argv[1])
+    else:
+        req_path = full_path(os.getcwd())
 
-            filepaths = get_filepaths(req_path)
-            print_filepaths(req_path, filepaths)
+    # build the dictionary of paths and statuses
+    path_and_status = {}
+    """ nod = no difference
+        add = added file
+        del = removed file
+        gon = gone forever
+    """
+    paths = scan_filepaths(req_path)
+    for path in paths:
+        path_and_status.update({path:"nod"})
 
-            while True:
-                time.sleep(0.25)
-                if (filepaths == get_filepaths(req_path)):
-                    None
-                else:
-                    break
+    # print the paths
+    pretty_print(path_and_status)
 
-    except KeyboardInterrupt: # sleep throws this exception if we ctrl+c
-        sys.exit(0)
+    while True:
+        # while in the loop, scan the filepaths again (new_paths) for a delta comparison
+        new_paths = scan_filepaths(req_path)
+
+        if paths == new_paths:
+            time.sleep(tick)
+        else:
+            # update the dictionary and path list
+            path_and_status = update_path_and_statuses(path_and_status, paths, new_paths)
+            paths = new_paths
+            pretty_print(path_and_status)
+            time.sleep(tick*10)
 
 if __name__ == "__main__":
     main()
